779d493535f6a4296c8351f09828c1d9fd040611
amit708091 <132252932+amit708091@users.noreply.github.com>
2023-07-10 18:00:11 +0530

    Update text2.php

--- text2.php
+++ text2.php
@@ -1,11 +1,1 @@
-<?php
-function familyName($fname) {
-  echo "$fname Refsnes.<br>";
-}
 
-familyName("Jani");
-familyName("Hege");
-familyName("Stale");
-familyName("Kai Jim");
-familyName("Borge");
-?>
